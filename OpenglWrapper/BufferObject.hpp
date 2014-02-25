440b1b17663c09c85d55e802c987ea13d80b5684
TiXiR <matti166@student.liu.se>
2014-02-25 21:16:33 +0100

    sadf

--- OpenglWrapper/BufferObject.hpp
+++ OpenglWrapper/BufferObject.hpp
@@ -13,11 +13,53 @@
 class BufferObject
 {
 public:
-    BufferObject();
-
+    BufferObject(Type type = Type::VertexBuffer, Usage usage = Usage::StaticDraw);
+	
+	enum Type
+    {
+        VertexBuffer        = GL_ARRAY_BUFFER,
+        IndexBuffer         = GL_ELEMENT_ARRAY_BUFFER,
+        PixelPackBuffer     = GL_PIXEL_PACK_BUFFER,
+        PixelUnpackBuffer   = GL_PIXEL_UNPACK_BUFFER
+    };
+    enum Usage
+    {
+        StreamRead          = GL_STREAM_READ,
+        StreamCopy          = GL_STREAM_COPY,
+        StreamDraw          = GL_STREAM_DRAW,
+        StaticRead          = GL_STATIC_READ,
+        StaticCopy          = GL_STATIC_COPY,
+        StaticDraw          = GL_STATIC_DRAW,
+        DynamicRead         = GL_DYNAMIC_READ,
+        DynamicCopy         = GL_DYNAMIC_COPY,
+        DynamicDraw         = GL_DYNAMIC_DRAW
+    };
+	
+	bool create();
+	void destroy();
+	bool isCreated();
+	void bind();
+	void release();
+	
+	int size();
+	
+	template <class DATA>
+	bool read(int offset, std::vector<DATA> data);
+	template <class DATA>
+	void write(int offset, std::vector<DATA> data);
+	template <class DATA>
+	void allocate(std::vector<DATA> data);
+    void allocate(int count);
+	
+	Type getType();
+	Usage getUsage();	
+    void setUsage(Usage usage);
 
 public:
-
+	GLuint openglHandler;
+	int dataCount;
+	Type type;
+	Usage usage;
 };
 
 #endif //BUFFER_OBJECT_HPP
--- OpenglWrapper/VertexArrayObject.hpp
+++ OpenglWrapper/VertexArrayObject.hpp
@@ -13,10 +13,16 @@
 class VertexArrayObject
 {
 public:
-    VertexArrayObject();
-    void create();
-    void bind();
-    void release();
+    VertexArrayObject()		{ openglHandler = -1; }
+    ~VertexArrayObject()	{ destroy(); }
+	
+    bool create()			{ glGenVertexArrays(1, &openglHandler); return true; }
+    void destroy()			{ glDeleteVertexArrays(1, &openglHandler); openglHandler = -1; }
+	bool isCreated()		{ return openglHandler != -1; return true; }
+    bool bind()				{ glBindVertexArray(openglHandler); return true; }	// Generate GL_INVALID_OPERATION if openglHandler is invalid
+    void release()			{ glBindVertexArray(0); }
+	
+	GLuint getID()			{ return openglHandler; }
 
 public:
     GLuint openglHandler;
