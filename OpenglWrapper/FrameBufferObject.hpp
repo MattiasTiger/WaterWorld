f2e5246b28444007fd2cac21470b56a22ee42491
Mattias Tiger <matti166@student.liu.se>
2014-02-26 14:38:19 +0100

    FBO

---
+++ OpenglWrapper/FrameBufferObject.hpp
@@ -0,0 +1,23 @@
+/*
+ *
+ */
+#ifndef FRAME_BUFFER_OBJECT_HPP
+#define FRAME_BUFFER_OBJECT_HPP
+
+#include "OpenGL.hpp"
+
+/* OpenGL Frame Buffer Object
+ * --------------------
+ * Purpose, Usage...
+ */
+class FrameBufferObject
+{
+public:
+    FrameBufferObject();
+
+
+public:
+    GLuint openglHandler;
+};
+
+#endif //FRAME_BUFFER_OBJECT_HPP
